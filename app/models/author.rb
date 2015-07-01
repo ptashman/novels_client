3d79f460687f953bc80efe8a5fea6e6e352b5529
Paul Tashman <paul.tashman@careerbuilder.com>
2015-07-01 15:09:53 -0400

    Adds test

--- app/models/author.rb
+++
@@ -1,2 +0,0 @@
-class Author < Base
-end
--- test/models/novel_test.rb
+++ test/models/novel_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class NovelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "does not save without a title" do
+    @novel_without_title = Novel.new(title: nil)
+    assert @novel_without_title.save == false
+  end
 end
